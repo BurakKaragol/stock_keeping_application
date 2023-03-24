2ed3b748c52fa2ccf52b00f80b9eb7dca7c828ec
Burak Karagol <brk.krgl.34@hotmail.com>
2023-03-24 09:56:29 +0300

    Add project files.

---
+++ Enumerators.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stock_keeping_application
+{
+    public enum ComponentType
+    {
+        Material,
+        HalfProduct,
+        Product
+    }
+
+    public enum StockPosition
+    {
+        Merkez,
+        Uretim1,
+        Uretim2,
+        Uretim3,
+        Uretim4
+    }
+
+    internal class Enumerators
+    {
+    }
+}
