2c7ec187f2d22bcb9005fd6b9764bacd6060ad63
Burak Karagol <brk.krgl.34@hotmail.com>
2023-03-27 14:55:51 +0300

    Update AmountControlForm.Designer.cs
    Added total and selective stock displayer. Needs function to operate

--- AmountControlForm.Designer.cs
+++ AmountControlForm.Designer.cs
@@ -31,6 +31,19 @@
             this.LowerPanel = new System.Windows.Forms.Panel();
             this.AmountDataGrid = new System.Windows.Forms.DataGridView();
             this.UpperPanel = new System.Windows.Forms.Panel();
+            this.ActionsGroup = new System.Windows.Forms.GroupBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.SelectedStockAmount = new System.Windows.Forms.Label();
+            this.SelectedStockPrice = new System.Windows.Forms.Label();
+            this.TotalStockPriceLabel = new System.Windows.Forms.Label();
+            this.TotalStockAmountLabel = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.FilterButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.FilterTextBox = new System.Windows.Forms.TextBox();
             this.OptionsGroup = new System.Windows.Forms.GroupBox();
             this.UpdateTableButton = new System.Windows.Forms.Button();
             this.EditStockButton = new System.Windows.Forms.Button();
@@ -52,16 +65,13 @@
             this.CurrentStockLabel = new System.Windows.Forms.Label();
             this.StockIDLabel = new System.Windows.Forms.Label();
             this.StockIdTextBox = new System.Windows.Forms.TextBox();
-            this.ActionsGroup = new System.Windows.Forms.GroupBox();
-            this.FilterTextBox = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.FilterButton = new System.Windows.Forms.Button();
             this.LowerPanel.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.AmountDataGrid)).BeginInit();
             this.UpperPanel.SuspendLayout();
+            this.ActionsGroup.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
             this.OptionsGroup.SuspendLayout();
             this.SelectedMaterialProperitiesGroup.SuspendLayout();
-            this.ActionsGroup.SuspendLayout();
             this.SuspendLayout();
             // 
             // LowerPanel
@@ -73,7 +83,7 @@
             this.LowerPanel.Controls.Add(this.AmountDataGrid);
             this.LowerPanel.Location = new System.Drawing.Point(0, 156);
             this.LowerPanel.Name = "LowerPanel";
-            this.LowerPanel.Size = new System.Drawing.Size(884, 305);
+            this.LowerPanel.Size = new System.Drawing.Size(883, 356);
             this.LowerPanel.TabIndex = 0;
             // 
             // AmountDataGrid
@@ -85,7 +95,7 @@
             this.AmountDataGrid.Location = new System.Drawing.Point(0, 0);
             this.AmountDataGrid.Name = "AmountDataGrid";
             this.AmountDataGrid.ReadOnly = true;
-            this.AmountDataGrid.Size = new System.Drawing.Size(884, 305);
+            this.AmountDataGrid.Size = new System.Drawing.Size(883, 356);
             this.AmountDataGrid.TabIndex = 0;
             this.AmountDataGrid.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.AmountDataGrid_CellClick);
             // 
@@ -98,8 +108,152 @@
             this.UpperPanel.Controls.Add(this.SelectedMaterialProperitiesGroup);
             this.UpperPanel.Location = new System.Drawing.Point(0, 0);
             this.UpperPanel.Name = "UpperPanel";
-            this.UpperPanel.Size = new System.Drawing.Size(884, 150);
+            this.UpperPanel.Size = new System.Drawing.Size(883, 150);
             this.UpperPanel.TabIndex = 1;
+            // 
+            // ActionsGroup
+            // 
+            this.ActionsGroup.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ActionsGroup.Controls.Add(this.tableLayoutPanel1);
+            this.ActionsGroup.Controls.Add(this.FilterButton);
+            this.ActionsGroup.Controls.Add(this.label1);
+            this.ActionsGroup.Controls.Add(this.FilterTextBox);
+            this.ActionsGroup.Dock = System.Windows.Forms.DockStyle.Right;
+            this.ActionsGroup.Location = new System.Drawing.Point(433, 0);
+            this.ActionsGroup.Name = "ActionsGroup";
+            this.ActionsGroup.Size = new System.Drawing.Size(250, 150);
+            this.ActionsGroup.TabIndex = 20;
+            this.ActionsGroup.TabStop = false;
+            this.ActionsGroup.Text = "Actions";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.Controls.Add(this.SelectedStockAmount, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.SelectedStockPrice, 2, 2);
+            this.tableLayoutPanel1.Controls.Add(this.TotalStockPriceLabel, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.TotalStockAmountLabel, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label4, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.label3, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.comboBox1, 2, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(9, 74);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(235, 70);
+            this.tableLayoutPanel1.TabIndex = 37;
+            // 
+            // SelectedStockAmount
+            // 
+            this.SelectedStockAmount.AutoSize = true;
+            this.SelectedStockAmount.Location = new System.Drawing.Point(139, 25);
+            this.SelectedStockAmount.Name = "SelectedStockAmount";
+            this.SelectedStockAmount.Size = new System.Drawing.Size(34, 13);
+            this.SelectedStockAmount.TabIndex = 41;
+            this.SelectedStockAmount.Text = "0.000";
+            this.SelectedStockAmount.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // SelectedStockPrice
+            // 
+            this.SelectedStockPrice.AutoSize = true;
+            this.SelectedStockPrice.Location = new System.Drawing.Point(139, 50);
+            this.SelectedStockPrice.Name = "SelectedStockPrice";
+            this.SelectedStockPrice.Size = new System.Drawing.Size(34, 13);
+            this.SelectedStockPrice.TabIndex = 42;
+            this.SelectedStockPrice.Text = "0.000";
+            this.SelectedStockPrice.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // TotalStockPriceLabel
+            // 
+            this.TotalStockPriceLabel.AutoSize = true;
+            this.TotalStockPriceLabel.Location = new System.Drawing.Point(52, 50);
+            this.TotalStockPriceLabel.Name = "TotalStockPriceLabel";
+            this.TotalStockPriceLabel.Size = new System.Drawing.Size(34, 13);
+            this.TotalStockPriceLabel.TabIndex = 40;
+            this.TotalStockPriceLabel.Text = "0.000";
+            this.TotalStockPriceLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // TotalStockAmountLabel
+            // 
+            this.TotalStockAmountLabel.AutoSize = true;
+            this.TotalStockAmountLabel.Location = new System.Drawing.Point(52, 25);
+            this.TotalStockAmountLabel.Name = "TotalStockAmountLabel";
+            this.TotalStockAmountLabel.Size = new System.Drawing.Size(34, 13);
+            this.TotalStockAmountLabel.TabIndex = 39;
+            this.TotalStockAmountLabel.Text = "0.000";
+            this.TotalStockAmountLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(3, 50);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(31, 13);
+            this.label4.TabIndex = 38;
+            this.label4.Text = "Price";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 25);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 13);
+            this.label3.TabIndex = 37;
+            this.label3.Text = "Amount";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(52, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 13);
+            this.label2.TabIndex = 33;
+            this.label2.Text = "TOTAL STOCK";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(139, 3);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(79, 21);
+            this.comboBox1.TabIndex = 36;
+            // 
+            // FilterButton
+            // 
+            this.FilterButton.Location = new System.Drawing.Point(9, 46);
+            this.FilterButton.Name = "FilterButton";
+            this.FilterButton.Size = new System.Drawing.Size(238, 22);
+            this.FilterButton.TabIndex = 32;
+            this.FilterButton.Text = "Filter List";
+            this.FilterButton.UseVisualStyleBackColor = true;
+            this.FilterButton.Click += new System.EventHandler(this.FilterButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 13);
+            this.label1.TabIndex = 22;
+            this.label1.Text = "STOCK_ID";
+            // 
+            // FilterTextBox
+            // 
+            this.FilterTextBox.Location = new System.Drawing.Point(72, 19);
+            this.FilterTextBox.Name = "FilterTextBox";
+            this.FilterTextBox.Size = new System.Drawing.Size(175, 20);
+            this.FilterTextBox.TabIndex = 12;
+            this.FilterTextBox.TextChanged += new System.EventHandler(this.FilterTextBox_TextChanged);
+            this.FilterTextBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.FilterTextBox_KeyDown);
             // 
             // OptionsGroup
             // 
@@ -110,7 +264,7 @@
             this.OptionsGroup.Controls.Add(this.DeleteSelectedButton);
             this.OptionsGroup.Controls.Add(this.AddNewButton);
             this.OptionsGroup.Dock = System.Windows.Forms.DockStyle.Right;
-            this.OptionsGroup.Location = new System.Drawing.Point(684, 0);
+            this.OptionsGroup.Location = new System.Drawing.Point(683, 0);
             this.OptionsGroup.Name = "OptionsGroup";
             this.OptionsGroup.Size = new System.Drawing.Size(200, 150);
             this.OptionsGroup.TabIndex = 30;
@@ -188,7 +342,7 @@
             this.SelectedMaterialProperitiesGroup.Controls.Add(this.StockIdTextBox);
             this.SelectedMaterialProperitiesGroup.Location = new System.Drawing.Point(0, 0);
             this.SelectedMaterialProperitiesGroup.Name = "SelectedMaterialProperitiesGroup";
-            this.SelectedMaterialProperitiesGroup.Size = new System.Drawing.Size(428, 150);
+            this.SelectedMaterialProperitiesGroup.Size = new System.Drawing.Size(427, 150);
             this.SelectedMaterialProperitiesGroup.TabIndex = 10;
             this.SelectedMaterialProperitiesGroup.TabStop = false;
             this.SelectedMaterialProperitiesGroup.Text = "Selected Material Properities";
@@ -199,7 +353,7 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.TotalPriceTextBox.Location = new System.Drawing.Point(322, 71);
             this.TotalPriceTextBox.Name = "TotalPriceTextBox";
-            this.TotalPriceTextBox.Size = new System.Drawing.Size(100, 20);
+            this.TotalPriceTextBox.Size = new System.Drawing.Size(99, 20);
             this.TotalPriceTextBox.TabIndex = 16;
             this.TotalPriceTextBox.TextChanged += new System.EventHandler(this.TotalPriceTextBox_TextChanged);
             // 
@@ -209,7 +363,7 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.TotalStockTextBox.Location = new System.Drawing.Point(322, 45);
             this.TotalStockTextBox.Name = "TotalStockTextBox";
-            this.TotalStockTextBox.Size = new System.Drawing.Size(100, 20);
+            this.TotalStockTextBox.Size = new System.Drawing.Size(99, 20);
             this.TotalStockTextBox.TabIndex = 14;
             this.TotalStockTextBox.TextChanged += new System.EventHandler(this.TotalStockTextBox_TextChanged);
             // 
@@ -227,7 +381,7 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.AmountTextBox.Location = new System.Drawing.Point(347, 19);
             this.AmountTextBox.Name = "AmountTextBox";
-            this.AmountTextBox.Size = new System.Drawing.Size(75, 20);
+            this.AmountTextBox.Size = new System.Drawing.Size(74, 20);
             this.AmountTextBox.TabIndex = 12;
             this.AmountTextBox.TextChanged += new System.EventHandler(this.AmountTextBox_TextChanged);
             // 
@@ -274,7 +428,7 @@
             this.DescriptionTextBox.Location = new System.Drawing.Point(95, 97);
             this.DescriptionTextBox.Multiline = true;
             this.DescriptionTextBox.Name = "DescriptionTextBox";
-            this.DescriptionTextBox.Size = new System.Drawing.Size(327, 47);
+            this.DescriptionTextBox.Size = new System.Drawing.Size(326, 47);
             this.DescriptionTextBox.TabIndex = 17;
             this.DescriptionTextBox.TextChanged += new System.EventHandler(this.DescriptionTextBox_TextChanged);
             // 
@@ -321,53 +475,11 @@
             this.StockIdTextBox.TabIndex = 11;
             this.StockIdTextBox.TextChanged += new System.EventHandler(this.StockIdTextBox_TextChanged);
             // 
-            // ActionsGroup
-            // 
-            this.ActionsGroup.BackColor = System.Drawing.SystemColors.ControlLight;
-            this.ActionsGroup.Controls.Add(this.FilterButton);
-            this.ActionsGroup.Controls.Add(this.label1);
-            this.ActionsGroup.Controls.Add(this.FilterTextBox);
-            this.ActionsGroup.Dock = System.Windows.Forms.DockStyle.Right;
-            this.ActionsGroup.Location = new System.Drawing.Point(434, 0);
-            this.ActionsGroup.Name = "ActionsGroup";
-            this.ActionsGroup.Size = new System.Drawing.Size(250, 150);
-            this.ActionsGroup.TabIndex = 20;
-            this.ActionsGroup.TabStop = false;
-            this.ActionsGroup.Text = "Actions";
-            // 
-            // FilterTextBox
-            // 
-            this.FilterTextBox.Location = new System.Drawing.Point(72, 19);
-            this.FilterTextBox.Name = "FilterTextBox";
-            this.FilterTextBox.Size = new System.Drawing.Size(175, 20);
-            this.FilterTextBox.TabIndex = 12;
-            this.FilterTextBox.TextChanged += new System.EventHandler(this.FilterTextBox_TextChanged);
-            this.FilterTextBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.FilterTextBox_KeyDown);
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(6, 23);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(60, 13);
-            this.label1.TabIndex = 22;
-            this.label1.Text = "STOCK_ID";
-            // 
-            // FilterButton
-            // 
-            this.FilterButton.Location = new System.Drawing.Point(9, 48);
-            this.FilterButton.Name = "FilterButton";
-            this.FilterButton.Size = new System.Drawing.Size(238, 30);
-            this.FilterButton.TabIndex = 32;
-            this.FilterButton.Text = "Filter List";
-            this.FilterButton.UseVisualStyleBackColor = true;
-            this.FilterButton.Click += new System.EventHandler(this.FilterButton_Click);
-            // 
             // AmountControlForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(884, 461);
+            this.ClientSize = new System.Drawing.Size(884, 511);
             this.Controls.Add(this.UpperPanel);
             this.Controls.Add(this.LowerPanel);
             this.Name = "AmountControlForm";
@@ -376,11 +488,13 @@
             this.LowerPanel.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.AmountDataGrid)).EndInit();
             this.UpperPanel.ResumeLayout(false);
+            this.ActionsGroup.ResumeLayout(false);
+            this.ActionsGroup.PerformLayout();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
             this.OptionsGroup.ResumeLayout(false);
             this.SelectedMaterialProperitiesGroup.ResumeLayout(false);
             this.SelectedMaterialProperitiesGroup.PerformLayout();
-            this.ActionsGroup.ResumeLayout(false);
-            this.ActionsGroup.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -415,5 +529,14 @@
         private System.Windows.Forms.Button FilterButton;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox FilterTextBox;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label SelectedStockAmount;
+        private System.Windows.Forms.Label SelectedStockPrice;
+        private System.Windows.Forms.Label TotalStockPriceLabel;
+        private System.Windows.Forms.Label TotalStockAmountLabel;
     }
 }
